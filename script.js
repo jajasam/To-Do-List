e6e4ba640fdfb7c47f6741b81ac907742bd2413b
Jana <jana.samson@hotmail.fr>
2022-03-15 10:17:30 -0400

    use of a form

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
+const form = document.getElementById('form')
 const input = document.getElementById('new-elem-input');
 const listContainer = document.querySelector('.list-container');
 const list = document.querySelector('.list');
 
 let count = 0;
 
-document.addEventListener('keydown', (e) => {
-    if (e.keyCode == 13 && input.value) {
-        let todoEl = input.value;
-        // todoEl.textContent = '';
-        localStorage.setItem(count,todoEl)
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let todoEl = input.value;
+        let obj = {
+            "element": todoEl,
+            "checked": false
+        }
+
+        localStorage.setItem(count, JSON.stringify(obj))
         count++;
         input.value = '';
         displayList();
-}}
-)
+
+})
+
 
 displayList();
 
 function displayList() {
     list.textContent = '';
+
     for (const [key, value] in Object.entries(localStorage)) {
-        const elemToDo = localStorage.getItem(key)
+        const keyofElem = localStorage.getItem(key);
+        const elemToDo = JSON.parse(keyofElem);
+        // const {element, checked} = elemToDo;
+        // console.log()
 
         const label = document.createElement('label');
         label.id = count;
@@ -37,8 +49,9 @@ function displayList() {
         label.appendChild(radiobox);
         
         const p = document.createElement('p');
-        p.innerHTML = elemToDo;
-        label.appendChild(p)
+        // p.innerHTML = elemToDo;
+        // p.innerHTML = (`key`, JSON.parse(value));
+        label.appendChild(p);
 
         count++
     }
@@ -53,23 +66,6 @@ allToDoElements.forEach(elem => elem.addEventListener('click', function(e) {
     const icons = document.querySelectorAll('i');
     const arr = Array.from(icons)
     let i = arr.filter(icon => icon.id == idElem);
-    i[0].classList.toggle("fa-circle-o")
-    i[0].classList.toggle('fa-check-circle-o')
-    
-    
-    // (i.classList.contains('fa-circle-o') ? console.log('si papi') : console.log('nahh'));
-
-
-
-    // i.classList.add('fa-check-circle-o') && i.classList.remove('fa-circle-o') : 
-    // i.classList.add('fa-circle-o') && i.classList.remove('fa-check-circle-o'));
-
-
-
-    // console.log(icons.idElem)
-
-    // iconToChange.classList.contains('fa-circle-o') ? 
-    // iconToChange.classList.add('fa-check-circle-o') && iconToChange.classList.remove('fa-circle-o') : 
-    // iconToChange.classList.add('fa-circle-o') && iconToChange.classList.remove('fa-check-circle-o')
-
+    i[0].classList.toggle("fa-circle-o");
+    i[0].classList.toggle('fa-check-circle-o');
 }))
