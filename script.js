6ff4fde5a027c105485fa3775ec6899ec4c7eb71
Jana <jana.samson@hotmail.fr>
2022-03-14 22:01:45 -0400

    check done elements

--- script.js
+++ script.js
@@ -23,26 +23,53 @@ function displayList() {
         const elemToDo = localStorage.getItem(key)
 
         const label = document.createElement('label');
+        label.id = count;
         list.appendChild(label);
         
         const div = document.createElement('div');
         div.classList.add('to-do-elem');
         label.appendChild(div);
 
-        const radiobox = document.createElement('input');
-        radiobox.type = 'radio';
+        const radiobox = document.createElement('i');
+        radiobox.classList.add('fa');
+        radiobox.classList.add('fa-circle-o');
+        radiobox.id = count;
         label.appendChild(radiobox);
         
         const p = document.createElement('p');
         p.innerHTML = elemToDo;
         label.appendChild(p)
+
+        count++
     }
 }
 
 
-const allToDoElements = document.querySelectorAll('.to-do-elem');
+const allToDoElements = document.querySelectorAll('label');
 console.log(allToDoElements);
 
 allToDoElements.forEach(elem => elem.addEventListener('click', function(e) {
-    console.log(e.target)
+    const idElem = elem.id;
+    const icons = document.querySelectorAll('i');
+    const arr = Array.from(icons)
+    let i = arr.filter(icon => icon.id == idElem);
+    i[0].classList.toggle("fa-circle-o")
+    i[0].classList.toggle('fa-check-circle-o')
+    
+    
+    // (i.classList.contains('fa-circle-o') ? console.log('si papi') : console.log('nahh'));
+
+
+
+    // i.classList.add('fa-check-circle-o') && i.classList.remove('fa-circle-o') : 
+    // i.classList.add('fa-circle-o') && i.classList.remove('fa-check-circle-o'));
+
+
+
+    // console.log(icons.idElem)
+
+    // iconToChange.classList.contains('fa-circle-o') ? 
+    // iconToChange.classList.add('fa-check-circle-o') && iconToChange.classList.remove('fa-circle-o') : 
+    // iconToChange.classList.add('fa-circle-o') && iconToChange.classList.remove('fa-check-circle-o')
+
 }))
